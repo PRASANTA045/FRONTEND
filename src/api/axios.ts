2c1f6de65fd96bc67fe47c86630ce24a1fe69729
PRASANT NAYAK <mrprashant218@gmail.com>
2025-11-26 13:47:42 +0530

    some application code replace

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
 
-
-
-useEffect(() => {
-  console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
-}, []);
 
 
 const api = axios.create({
